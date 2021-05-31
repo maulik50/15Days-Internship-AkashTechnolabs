513dc5d275cc8062c9da4acd518636e873b65449
maulik50 <72292968+maulik50@users.noreply.github.com>
2021-05-31 11:54:18 +0530

    Add files via upload

---
+++ Day 3/LoginActivity.java
@@ -0,0 +1,33 @@
+package com.example.sampleapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class LoginActivity extends AppCompatActivity {
+
+    TextView creat_new_account;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        getSupportActionBar().setTitle("Login");
+
+        creat_new_account = findViewById(R.id.create_new_account);
+
+        creat_new_account.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(LoginActivity.this, RegisterActivity.class);
+                startActivity(intent);
+            }
+        });
+
+    }
+}
