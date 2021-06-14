0bbc8b6881e5f6a9c3c559a5a9b7292f925e013f
maulik50 <72292968+maulik50@users.noreply.github.com>
2021-06-14 22:47:39 +0530

    Update RegisterActivity.java

--- Day 4/RegisterActivity.java
+++ Day 4/RegisterActivity.java
@@ -5,23 +5,73 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
-public class RegisterActivity extends AppCompatActivity {
+public class SignupActivity extends AppCompatActivity {
 
-    TextView alreadyaccount;
+    EditText name,email,contact,password;
+    TextView alredyacc;
+    Button signup;
+
+    String emailPattern = "[a-zA-Z0-9._-]+@[a-z]+\\.+[a-z]+";
+    String passwordPattern = "(?=.*[0-9])(?=.*[a-z])(?=\\S+$).{6,10}";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_register);
+        setContentView(R.layout.activity_signup);
+        getSupportActionBar().setTitle("Signup");
 
-        alreadyaccount = findViewById(R.id.alreadyhaveaccount);
-        alreadyaccount.setOnClickListener(new View.OnClickListener() {
+        name =findViewById(R.id.signup_name);
+        email =findViewById(R.id.signup_mail);
+        contact =findViewById(R.id.signup_mobile);
+        password =findViewById(R.id.signup_password);
+        signup =findViewById(R.id.signup_btn);
+        alredyacc =findViewById(R.id.alredy_account);
+
+        signup.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                onBackPressed();
+                if(name.getText().toString().trim().equalsIgnoreCase("")){
+                    name.setError("Name Required");
+                }
+                else if(email.getText().toString().trim().equalsIgnoreCase("")){
+                    email.setError("Email Id Required");
+                }
+                else if(!email.getText().toString().trim().matches(emailPattern)){
+                    email.setError("Valid Email Id Required");
+                }
+                else if(contact.getText().toString().trim().equalsIgnoreCase("")){
+                    contact.setError("Contact No. Required");
+                }
+                else if(contact.getText().toString().length()<10){
+                    contact.setError("Valid Contact No. Required");
+                }
+                else if(password.getText().toString().trim().equalsIgnoreCase("")){
+                    password.setError("Password Required");
+                }
+                else if(!password.getText().toString().trim().matches(passwordPattern)){
+                    password.setError("Strong Password Required");
+                }
+                else {
+                    Intent intent = new Intent(SignupActivity.this, HomeActivity.class);
+                    startActivity(intent);
+                    Toast.makeText(SignupActivity.this, "Signup Successfully", Toast.LENGTH_LONG).show();
+                }
+
             }
         });
+
+        alredyacc.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(SignupActivity.this, LoginActivity.class);
+                startActivity(intent);
+            }
+        });
+
     }
 }
