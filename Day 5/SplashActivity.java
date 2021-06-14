aa77d165960bf846bb21e29c5326dddc5b447069
maulik50 <72292968+maulik50@users.noreply.github.com>
2021-06-14 22:54:18 +0530

    Add files via upload

---
+++ Day 5/SplashActivity.java
@@ -0,0 +1,35 @@
+package com.example.sampleapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.animation.AlphaAnimation;
+import android.widget.ImageView;
+
+public class SplashActivity extends AppCompatActivity {
+
+    ImageView imageview;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+        getSupportActionBar().hide();
+        imageview = findViewById(R.id.splash_icon);
+
+        AlphaAnimation animation = new AlphaAnimation(0,1);
+        animation.setDuration(2500);
+        imageview.startAnimation(animation);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(SplashActivity.this, LoginActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        }, 3000);
+    }
+}
